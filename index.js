292d2dc409178a8c369819fb99d2ad9c5ecd6213
Laurent Eyckermans <laurent.eyckermans@gmail.com>
2022-03-29 01:15:51 +0200

    importing Fct to the index

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const scrapUrls = require('./scrapping/raphaScrap');
+const georgianaScrap = require('./scrapping/georgianaScrap')
+const laurentSrap = require('./scrapping/laurentScrap')
+
+scrapUrls();
+georgianaScrap();
+laurentSrap();
+
