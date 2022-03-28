777d138cdfc92ea08da79d9808ffe9e76a3ccf9b
Laurent Eyckermans <laurent.eyckermans@gmail.com>
2022-03-28 16:59:16 +0200

    laurentFixPage 2

--- index2.js
+++ index2.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
+
+const scrapLaurent = async() =>{
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: false,
@@ -127,7 +128,10 @@ const puppeteer = require("puppeteer");
     
         */
 
+}
+
+module.exports = scrapLaurent;
 
 
-    })()
+    
 
