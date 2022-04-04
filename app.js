c83cd95dc3c22ec2b01622b735d6199dda3f739f
Georgiana Marcu <georgiana.marcu22@gmail.com>
2022-04-04 15:20:54 +0200

    First time deployment .env ignored and port set

--- app.js
+++ app.js
@@ -83,4 +83,4 @@ app.use('/lists', require('./routes/lists'));
 
 
 
-app.listen(3000, () => { console.log('Server started on port 3000') });
+app.listen(process.env.PORT || 3000, () => { console.log('Server started on port 3000') });
