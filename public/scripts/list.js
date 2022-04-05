5baafafbfb29cee8cea8141814e9d67eebe6d39d
clementv <vermylenclement@gmail.com>
2022-04-05 16:58:54 +0200

    working version of final shopping list

--- public/scripts/list.js
+++ public/scripts/list.js
@@ -1,8 +1,6 @@
     const header = document.querySelector('header')
-    const categories = ['Vegetables', 'Dairy', 'Meat']
-
-    const ingredientsToGet = ['carrot', 'apple', 'steak', 'maple syrup']
-    const ingredientsDone = []
+    const categories = [{img: ['fa-solid fa-carrot'], cat: 'Vegetables', list: ['carrot', 'apple']}, {img: ['fa-solid fa-bacon'], cat: 'Meat', list: ['Steak', 'Chicken', 'Sausage']}, {img: ['fa-solid fa-cow'], cat: 'Dairy', list: ['Milk', 'Butter']}]
+    const doneList = document.querySelector(".doneList")
 
     categories.forEach((category, index) => {
         header.insertAdjacentHTML('afterend', 
@@ -10,114 +8,121 @@
         <input type="checkbox" name="collapse" id="handle${index}" class="expand">
         <div class="list-header-container list-flex">
             <div class="list-food-img-container list-flex">
-                <i class="fa-solid fa-carrot"></i>
+                <i class="${category.img}"></i>
             </div>
             <label for="handle${index}">
-                <h1 class="list-food-name">${category}</h1>
+                <h1 class="list-food-name">${category.cat}</h1>
                 <i id='list-expand-arrow'class="fa-solid fa-angle-right arrow"></i>
             </label>
         </div>
         <div class="content list-ingredients-container list-flex">
-            <div class="list-ingredient list-flex">
-                <input type="checkbox" class="list-checkbox">
-                <p>${ingredientsToGet[index]}</p>
-            </div>
-            <div class="list-ingredient list-flex">
-                <input type="checkbox" class="list-checkbox">
-                <p>${ingredientsToGet[index]}</p>
-            </div>
-            <div class="list-ingredient list-flex">
-                <input type="checkbox" class="list-checkbox">
-                <p>${ingredientsToGet[index]}</p>
-            </div>
+            
         </div>
     </section>`)
+
+    category.list.forEach((listItem) => {
+        document.querySelector(".content.list-ingredients-container.list-flex").insertAdjacentHTML("beforeend", `<div class="list-ingredient list-checkbox list-flex">
+        <input type="checkbox">
+        <p>${listItem}</p>
+    </div>`)
+    })
+
+    category.list.forEach((listItem) => {
+        doneList.insertAdjacentHTML("beforeend", `<div class="list-ingredient list-checkbox-done list-flex">
+        <input type="checkbox">
+        <p>${listItem}</p>
+    </div>`)
     })
     
-    const ingredientSections = (document.querySelectorAll('.list-ingredient-category'))
-    const lastSection = ingredientSections[ingredientSections.length-1]
+    
 
-    lastSection.insertAdjacentHTML('afterend',
-    `<section class="accordion list-ingredient-done">
-    <input type="checkbox" name="collapse" id="handle99" class="expand">
-    <div class="list-header-container list-flex">
-        <div class="list-food-img-container list-flex">
-            <i class="fa-solid fa-check"></i>
-        </div>
-        <label for="handle99">
-            <h1 class="list-food-name">Done</h1>
-            <i id='list-expand-arrow'class="fa-solid fa-angle-right arrow"></i>
-        </label>
-    </div>
-    <div class="content list-ingredients-container list-flex">
-        <div class="list-ingredient list-flex">
+})
+    
+    
 
-        </div>
-        <div class="list-ingredient list-flex">
-        </div>
-        <div class="list-ingredient list-flex">
-        </div>
-    </div>
-    </section>`
-    )
+    const checkBoxEvent = () => {
+        const ingredientDiv = Array.from(document.querySelectorAll('div.list-checkbox'))
+        const doneDiv = Array.from(document.querySelectorAll('div.list-checkbox-done'))
 
+        ingredientDiv.forEach((id, index) => {
+            id.addEventListener('click', () => {
+                if (id.firstElementChild.checked || id.firstElementChild.checked===false) {
+                    doneDiv.forEach((dd) => {
+                        if(dd.lastElementChild.innerText === id.lastElementChild.innerText){
+                            dd.style.display = "flex"
+                            dd.firstElementChild.checked = true
+                            id.style.display = "none"
+                        }
+                    })
+                }
+            })
+        })
     
-    
-    
-    
-    const headerLabel = document.querySelectorAll('label')
+        doneDiv.forEach((dd, index) => {
+            dd.addEventListener('click', () => {
+                ingredientDiv.forEach((id) => {
+                    if(id.lastElementChild.innerText === dd.lastElementChild.innerText){
+                        id.style.display = "flex"
+                        id.firstElementChild.checked = false
+                        dd.style.display = "none"
+                    }
+                })
+            })
+        })
+    }
+   
+    checkBoxEvent()
+    const headerLabel = Array.from(document.querySelectorAll('label:not(.done, .homepage-hamb)'))
     const expandArrow = Array.from(document.querySelectorAll('i#list-expand-arrow'))
 
-    headerLabel.forEach((label, index) => {
-        label.addEventListener('click', () => {
-            if (expandArrow[index].classList.contains("fa-angle-right")) {
-                console.log(expandArrow[index].className)
-                expandArrow[index].classList.replace('fa-angle-right', 'fa-angle-down')
+    const arrowEvent = () => {
+        
+        headerLabel.forEach((label, index) => {
+            label.addEventListener('click', () => {
+                if (expandArrow[index].classList.contains("fa-angle-right")) {
+                    // console.log(expandArrow[index].className)
+                    expandArrow[index].classList.replace('fa-angle-right', 'fa-angle-down')
+                } 
+                else {
+                    // console.log(expandArrow[index].className)
+                    expandArrow[index].classList.replace('fa-angle-down', 'fa-angle-right')
+                }
+            })
+        })
+    }
+
+    const headerLabelDone = document.querySelector('label.done')
+    const expandArrowDone = document.querySelector('i#list-expand-arrow-done')
+
+    const arrowDoneEvent = () => {
+
+        headerLabelDone.addEventListener('click', () => {
+            if (expandArrowDone.classList.contains("fa-angle-right")) {
+                // console.log(expandArrowDone.className)
+                expandArrowDone.classList.replace('fa-angle-right', 'fa-angle-down')
             } 
             else {
-                console.log(expandArrow[index].className)
-                expandArrow[index].classList.replace('fa-angle-down', 'fa-angle-right')
+                // console.log(expandArrowDone.className)
+                expandArrowDone.classList.replace('fa-angle-down', 'fa-angle-right')
             }
         })
-    })
+    }
+
+    arrowEvent()
+    arrowDoneEvent()
+    
+  
+    
+    
+
+    
+        
+
+
+        
 
 
 
-    const ingredientCheckboxes = Array.from(document.querySelectorAll('input.list-checkbox'))
-    const doneCheckboxes = Array.from(document.querySelectorAll('input.list-checkbox-done'))
-    const doneSection = Array.from(document.querySelectorAll('.list-ingredient-done .list-ingredient'))
 
-    ingredientCheckboxes.forEach((checkbox, index) => {
-        checkbox.addEventListener('click', () => {
-            if (checkbox.checked) {
-                ingredientsDone.push(checkbox.nextElementSibling.innerText)
-                ingredientsToGet.pop(checkbox.nextElementSibling.innerText)
-                checkbox.parentElement.innerHTML = ''
-                doneSection[index].innerHTML = `<input type="checkbox" class="list-checkbox-done" checked>
-                <p>${ingredientsDone}</p>`
-                console.log(ingredientsToGet)
-                console.log(ingredientsDone)  
-            }
-            else {
-                ingredientsToGet.push(checkbox.nextElementSibling.innerText)
-                ingredientsDone.pop(checkbox.nextElementSibling.innerText)
-                console.log(ingredientsToGet)
-                console.log(ingredientsDone)  
-            }
-        })
-    })
 
-    doneCheckboxes.forEach((checkbox, index) => {
-        checkbox.addEventListener('click', () => {
-            if (checkbox.checked === false) {
-                ingredientsToGet.push(checkbox.nextElementSibling.innerText)
-                ingredientsDone.pop(checkbox.nextElementSibling.innerText)
-                checkbox.parentElement.innerHTML = ''
-                ingredientSections.innerHTML = `<input type="checkbox" class="list-checkbox">
-                <p>${ingredientsToGet}</p>`
-                console.log(ingredientsToGet)
-                console.log(ingredientsDone)  
-            }
-        })
-    })
     
